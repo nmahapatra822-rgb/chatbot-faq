13d1fe99639ad5588981539dd84106e1f15f0903
nmahapatra822-rgb <nmahapatra822@gmail.com>
2026-07-05 16:37:42 +0530

    Refactor text preprocessing and add NLTK downloads
    Updated text preprocessing function and added NLTK downloads.

--- utils.py
+++ utils.py
@@ -7,45 +7,41 @@ from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# Download NLTK resources (first time only)
+
 nltk.download('punkt')
 nltk.download('punkt_tab')
 nltk.download('stopwords')
 
-# Load FAQ dataset
+
 df = pd.read_csv("Data/faq.csv")
 
 
-# -----------------------------
-# Text Preprocessing Function
-# -----------------------------
+
 def preprocess(text):
     text = text.lower()
 
-    # Remove punctuation
+   
     text = text.translate(str.maketrans('', '', string.punctuation))
 
-    # Tokenize
+    
     words = word_tokenize(text)
 
-    # Remove stopwords
+    
     stop_words = set(stopwords.words("english"))
     words = [word for word in words if word not in stop_words]
 
     return " ".join(words)
 
 
-# Preprocess all questions
+
 df["Processed"] = df["Question"].apply(preprocess)
 
-# TF-IDF Vectorizer
+
 vectorizer = TfidfVectorizer()
 question_vectors = vectorizer.fit_transform(df["Processed"])
 
 
-# -----------------------------
-# Get Answer Function
-# -----------------------------
+
 def get_answer(user_question):
 
     processed = preprocess(user_question)
